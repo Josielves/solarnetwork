c054fca47663013f33f1c51a9bfab39dd5dcdc2c
Josielves <josielves.silva@gmail.com>
2026-06-11 09:01:57 -0300

    Removendo todos os arquivos

--- backend/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import 'dotenv/config';
-
-export const sb = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  { auth: { autoRefreshToken: false, persistSession: false } }
-);
