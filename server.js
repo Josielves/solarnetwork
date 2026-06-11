3fe34202fe955bc0f26baffbc974fbad0158bbef
Josielves <josielves.silva@gmail.com>
2026-06-11 10:04:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app  = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get("*", (req, res) => {
